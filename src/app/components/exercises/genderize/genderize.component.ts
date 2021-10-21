430b3f0f2caa9503c2978f4ae0041267d79b2738
ClaudioAlarcon <calarcon1992@gmail.com>
2021-10-21 15:30:59 -0300

    genderImage auto changing

--- src/app/components/exercises/genderize/genderize.component.ts
+++ src/app/components/exercises/genderize/genderize.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Genderize } from 'src/app/interfaces/genderize';
 import { EndpointService } from 'src/app/services/endpoint.service';
 
@@ -9,17 +9,27 @@ import { EndpointService } from 'src/app/services/endpoint.service';
 })
 export class GenderizeComponent implements OnInit {
   public data: any;
-  public gender: any;
+  public gender: string;
+  public url: string;
+  public name: string;
 
-  constructor(public endpoint: EndpointService) { }
+  constructor(public endpoint: EndpointService) { 
+    this.gender = '';
+    this.url = '';
+    this.name= '';
+  }
 
   ngOnInit(): void {
   }
-
+  
   public getEndpointData(): void {
+    this.name = (<HTMLInputElement>document.getElementById("name")).value;
     this.data = GenderizeComponent;
-    this.endpoint.getData('https://api.genderize.io/?name=claudio').subscribe((res: Genderize) => {
+    this.endpoint.getData('https://api.genderize.io/?name='+this.name).subscribe((res: Genderize) => {
     this.data = res;
+    console.log(this.data);
+    this.gender = res.gender;
+    this.url = 'assets/genderize/'+this.gender+'.jpg';
   });
 }
 
