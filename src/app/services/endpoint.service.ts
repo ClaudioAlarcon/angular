e2b55abd0b6054d26e5cde18d01c6a9932925afa
ClaudioAlarcon <calarcon1992@gmail.com>
2021-10-20 21:07:14 -0300

    getData function created

--- src/app/services/endpoint.service.ts
+++ src/app/services/endpoint.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EndpointService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getData(url: string): Observable<any> {
+    return this.http.get(url);
+  }
 }
