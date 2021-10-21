16a6e4ac1678dcfd338e81502cc3e5866d59b5f4
ClaudioAlarcon <calarcon1992@gmail.com>
2021-10-21 19:03:14 -0300

    donutchart html added

--- src/app/components/exercises/widgets/donutchart/donutchart.component.ts
+++ src/app/components/exercises/widgets/donutchart/donutchart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-donutchart',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonutchartComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+    var myChart = new Chart("myChart", {
+      type: 'doughnut',
+      data: {
+          labels: ['Female', 'Male'],
+          datasets: [{
+              label: 'My First Dataset',
+              data: [90, 10],
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+              ],
+              hoverOffset: 4
+          }]
+      }
+  });
   }
-
 }
