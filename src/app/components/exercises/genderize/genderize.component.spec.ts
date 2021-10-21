86b4c3ae371a330cdf697a9ec480a4e16a629421
ClaudioAlarcon <calarcon1992@gmail.com>
2021-10-20 22:18:27 -0300

    genderize component created

---
+++ src/app/components/exercises/genderize/genderize.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GenderizeComponent } from './genderize.component';
+
+describe('GenderizeComponent', () => {
+  let component: GenderizeComponent;
+  let fixture: ComponentFixture<GenderizeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GenderizeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GenderizeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
