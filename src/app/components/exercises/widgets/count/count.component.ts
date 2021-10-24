0f4d5ec8ea9b4be6be02a39c463e1de569d9c544
ClaudioAlarcon <calarcon1992@gmail.com>
2021-10-24 00:29:35 -0300

    Count Component added

--- src/app/components/exercises/widgets/count/count.component.ts
+++ src/app/components/exercises/widgets/count/count.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-count',
   templateUrl: './count.component.html',
   styleUrls: ['./count.component.css']
 })
-export class CountComponent implements OnInit {
-
+export class CountComponent{
+  @Input() count!: number;
+  
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
